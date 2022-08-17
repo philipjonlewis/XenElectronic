b9e69e1a7c04c1c49043cf2859f2b58a554b749b
Philip Lewis <philipjonlewis@gmail.com>
2022-08-17 15:58:57 +0800

    fixed serer env error

--- server/src/database/dbConnection.ts
+++ server/src/database/dbConnection.ts
@@ -1,23 +1,23 @@
-import mongoose from "mongoose";
-import path from "path";
-import { config } from "../config";
-require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+import mongoose from 'mongoose';
+import path from 'path';
+import { config } from '../config';
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const mongooseOptions: any = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
 const dbPort: any =
-  config.environment == "development"
+  config.environment == 'development'
     ? process.env.DB_PORT_DEVELOPMENT
     : process.env.DB_PORT_PRODUCTION;
 
 const databaseConnection = async () => {
   try {
     mongoose.connect(dbPort, mongooseOptions);
-    console.log("Connected to the database");
+    console.log('Connected to the database');
   } catch (error) {
-    console.log("Unable to Connect to the database", error);
+    console.log('Unable to Connect to the database', error);
   }
 };
 
