c881d0499485107c343893f0939ad89a3fffb2e6
Philip Lewis <philipjonlewis@gmail.com>
2022-08-18 13:19:42 +0800

    weird typescript error

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response, NextFunction } from 'express';
+import express, { Express, Request, response, NextFunction } from 'express';
 
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
