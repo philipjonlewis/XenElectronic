94401537baa796bcd449b9f055eccbaa97761320
Philip Lewis <philipjonlewis@gmail.com>
2022-08-19 02:01:20 +0800

    trial again

--- server/src/app.ts
+++ server/src/app.ts
@@ -52,8 +52,10 @@ app.use(nocache());
 app.use(function (req, res, next) {
   res.header('Content-Type', 'application/json;charset=UTF-8');
   // res.header("Access-Control-Allow-Credentials", "*");
-  res.header('Access-Control-Allow-Credentials', process.env.FRONTEND_PORT);
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Credentials', "https://xenelectronicbyphiliplewis.netlify.app/");
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Origin', 'https://xenelectronicbyphiliplewis.netlify.app/');
+  res.header('Access-Control-Allow-Methods', 'POST,GET,PATCH,DELETE');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
