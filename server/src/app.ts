0de50cb063cbcc1c3be94dbf5f0fd0e9ae00547e
Philip Lewis <philipjonlewis@gmail.com>
2022-08-18 13:54:13 +0800

    Still fixing sudden ts error

--- server/src/app.ts
+++ server/src/app.ts
@@ -49,7 +49,7 @@ app.use(
   })
 );
 
-app.use((req: Request, res: Response, next: NextFunction) => {
+app.use((req, res, next: NextFunction) => {
   res.header('Content-Type', 'application/json;charset=UTF-8');
   res.header('Access-Control-Allow-Credentials', 'http://192.168.0.25:3000');
   // res.header('Access-Control-Allow-Credentials', process.env.FRONTEND_PORT);
